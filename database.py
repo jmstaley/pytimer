502c8175ba02f2762c32bc3a228c40110d06cbf4
Jon <jmstaley>
2009-06-05 00:50:44 +0100

    Initial work on database, using sqlite

---
+++ database.py
@@ -0,0 +1,47 @@
+import sqlite3
+import os.path
+
+class Database:
+    """ Class to handle saving/gettting
+        records for database
+    """
+
+    def __init__(self):
+        self.conn = None
+        self.db_file = '%s/.db_pytime' % os.path.expanduser('~')
+        self._create_tables()
+
+    def _create_tables(self):
+        """ create tables
+        """
+        if not self._table_exists(name='activities'):
+            cursor = self.conn.cursor()
+            cursor.execute('''create table activities
+             (start text, end text, description text)''')
+            self.conn.commit()
+            cursor.close()
+            self.conn.close()
+
+    def _table_exists(self, name=''):
+        """ test if table exists
+        """
+        exists = False
+        self.open()
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute('SELECT * FROM ?', name)
+            exists = True
+        except:
+            pass
+        cursor.close()
+        return exists
+
+    def open(self):
+        """ Open database file
+        """
+        self.conn = sqlite3.connect(self.db_file)
+    
+    def get_cursor(self):
+        """
+        """
+        return self.conn.cursor()
